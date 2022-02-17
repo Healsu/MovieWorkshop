41947ce82e16a7610e8f105d11dc64cc3cd8c749
Healsu <timmie2500@gmail.com>
2022-02-17 18:41:39 +0100

    Changes

--- src/main/java/com/example/movieworkshop/controller/Controls.java
+++ src/main/java/com/example/movieworkshop/controller/Controls.java
@@ -3,7 +3,6 @@ package com.example.movieworkshop.controller;
 import com.example.movieworkshop.models.Movie;
 import com.example.movieworkshop.repositories.MovieRepository;
 import com.example.movieworkshop.services.MovieService;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +20,33 @@ public class Controls {
 
 
    @GetMapping("/getFirst")
-    public String showMovies(){
+    public String showMovie(){
 
        Movie firstMovie = Service.getFirstMovie(Repository.convertFile());
 
     return firstMovie.toString();
 }
+
+    @GetMapping("/getRandom")
+    public String showRandMovie(){
+
+        Movie randomMovie = Service.getRandomMovie(Repository.convertFile());
+
+        return randomMovie.toString();
+    }
+
+    @GetMapping("/getTenSortByPopularity")
+    public String showTenPopularity(){
+
+        Movie firstMovie = Service.getSortedPopularity(Repository.convertFile());
+        return null;
+    }
+
+    @GetMapping("/howManyWonAwards")
+    public String showAwardsGotten(){
+
+        int movieAwards = Service.gotAward(Repository.convertFile());
+
+        return "The amount of movie awards gotten is "+movieAwards;
+    }
 }
--- src/main/java/com/example/movieworkshop/services/MovieService.java
+++ src/main/java/com/example/movieworkshop/services/MovieService.java
@@ -3,11 +3,55 @@ package com.example.movieworkshop.services;
 import com.example.movieworkshop.models.Movie;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class MovieService {
 
+    //Show the first movie on the list
     public Movie getFirstMovie(ArrayList<Movie> movieList){
         return movieList.get(0);
     }
 
+    //Get a random movie from the list
+    public Movie getRandomMovie(ArrayList<Movie> movieList){
+        Random random = new Random();
+        int limit = 100;
+        int randomNumber = random.nextInt(limit);
+
+        return movieList.get(randomNumber);
+
+    }
+
+
+    public Movie getSortedPopularity(ArrayList<Movie> movieList){
+
+        for (int i = 0; i < movieList.size(); i++) {
+            movieList.get(i).getPopularity();
+        }
+        return null;
+    }
+
+    //Show how many movies got an award from the list
+    public int gotAward(ArrayList<Movie> movieList){
+        int awards = 0;
+
+        for (int i = 0; i < movieList.size(); i++) {
+
+            if (movieList.get(i).isWonAnAward() == true){
+                awards++;
+            }
+        }
+        return awards;
+    }
+
+    public ArrayList<Movie> showWhoGotAward(ArrayList<Movie> movieList){
+        ArrayList<Movie> awardList = new ArrayList<>();
+        for (int i = 0; i < movieList.size(); i++) {
+            if (movieList.get(i).isWonAnAward() == true){
+                awardList.add(movieList.get(i));
+            }
+        }
+        return awardList;
+    }
 }
